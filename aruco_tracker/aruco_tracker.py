65ae73d3fb27a56d1dfa23229c498f1ca54fa973
Base02 <Base02@pensasystems.com>
2018-07-03 15:08:18 -0500

    Rotate Roll 180*

--- aruco_tracker/aruco_tracker.py
+++ aruco_tracker/aruco_tracker.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Int16
 from tf.msg import tfMessage
 from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_multiply
 import numpy as np
 import cv2
 import cv2.aruco as aruco
@@ -16,11 +17,11 @@ with np.load(rospack.get_path('aruco_tracker')+'/aruco_tracker/calibration_files
     mtx, dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
 
 cap = cv2.VideoCapture(1)
-    
+
 def aruco_tracker():
     pubPose = rospy.Publisher('/aruco_tracker/pose', PoseStamped, queue_size=1 )
     pubID = rospy.Publisher('/aruco_tracker/droneID', Int16 , queue_size=1 )
-    
+
     rospy.init_node('aruco_tracker', anonymous=True)
     rate = rospy.Rate(30) # 10hz
 
@@ -36,7 +37,7 @@ def aruco_tracker():
             #Define ArUco dictionary
             aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
             parameters = aruco.DetectorParameters_create()
-            
+
             #Detect Markers
             corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
@@ -46,18 +47,21 @@ def aruco_tracker():
 
                 #RPY to Quaternion Conversion
                 q = quaternion_from_euler(rvec[0][0][1],rvec[0][0][2],rvec[0][0][0])
-                
-                #Build poseStamped
+		#Rotate Roll 180 Degrees
+		q_rot = quaternion_from_euler(3.16, 0, 0)
+		q_new = quaternion_multiply(q_rot, q)
+
+		#Build poseStamped
                 poseStamped=PoseStamped()
                 poseStamped.header.frame_id = "/vislam"
                 poseStamped.header.stamp = rospy.Time.now()
                 poseStamped.pose.position.x = float(tvec[0][0][1]*-1)
                 poseStamped.pose.position.y = float(tvec[0][0][0])
                 poseStamped.pose.position.z = float(tvec[0][0][2])
-                poseStamped.pose.orientation.x = q[0]
-                poseStamped.pose.orientation.y = q[1]
-                poseStamped.pose.orientation.z = q[2]   
-                poseStamped.pose.orientation.w = q[3]
+                poseStamped.pose.orientation.x = q_new[0]
+                poseStamped.pose.orientation.y = q_new[1]
+                poseStamped.pose.orientation.z = q_new[2]
+                poseStamped.pose.orientation.w = q_new[3]
 
                 droneID = int(ids[0])
 
